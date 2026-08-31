ebe0db3d98903c1af209059912ad9060ff54d367
Rama Rante Lalo <ramarantelalo@gmail.com>
2026-08-31 08:13:43 +0700

    add home row mods with zmk-helper

---
+++ config/split34-labels.h
@@ -0,0 +1,40 @@
+// Left half
+#define LT0 0   // Q
+#define LT1 1   // W
+#define LT2 2   // E
+#define LT3 3   // R
+#define LT4 4   // T
+#define LM0 10  // A
+#define LM1 11  // S
+#define LM2 12  // D
+#define LM3 13  // F
+#define LM4 14  // G
+#define LB0 20  // Z
+#define LB1 21  // X
+#define LB2 22  // C
+#define LB3 23  // V
+#define LB4 24  // B
+#define LH0 30  // studio_unlock
+#define LH1 31  // space (left thumb)
+
+// Right half
+#define RT0 5   // Y
+#define RT1 6   // U
+#define RT2 7   // I
+#define RT3 8   // O
+#define RT4 9   // P
+#define RM0 15  // H
+#define RM1 16  // J
+#define RM2 17  // K
+#define RM3 18  // L
+#define RM4 19  // ESC
+#define RB0 25  // N
+#define RB1 26  // M
+#define RB2 27  // COMMA
+#define RB3 28  // DOT
+#define RB4 29  // TAB
+#define RH0 32  // space (right thumb)
+#define RH1 33  // none
+
+#define LEFT_HAND_KEYS  LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB0 LB1 LB2 LB3 LB4 LH0 LH1
+#define RIGHT_HAND_KEYS RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4 RH0 RH1
